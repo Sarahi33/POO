216898d7819c39763d253ffb524e7856d16219c2
Sarahi33 <99031663+Sarahi33@users.noreply.github.com>
2022-02-25 22:35:10 -0600

    Update Comparador.java

--- PrimerPeriodo/src/main/java/Hospital/Comparador.java
+++ PrimerPeriodo/src/main/java/Hospital/Comparador.java
@@ -4,7 +4,16 @@ package Hospital;
 /**
  *
  * @author Leonidas Adrian Mendoza Flores
+ *         Sarahi Guadalupe Silva Molina
  */
-public interface Comparador {
+public interface Comparador { //declaramos la interface llamada Comparador
+    public boolean igualQue(Object o);
     
+    public boolean menorIgual(Object o);
+    
+    public boolean menorQue(Object o);
+    
+    public boolean mayorIgual(Object o);
+    
+    public boolean mayorQue(Object o);
 }
